2b250d7aee22656234d0f56b09afd30b45173150
jinxuemei <1835355230@qq.com>
2020-10-17 15:11:15 +0800

    webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    //入口文件
+    entry:{
+        index: './src/index.js'
+    },
+    //出口
+    output:{
+        //绝对路径
+        path:path.resolve(__dirname,'dist'),//取本文件路径，再找同级文件
+        filename:'[name].js'//name取入口文件名
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    }
+}
